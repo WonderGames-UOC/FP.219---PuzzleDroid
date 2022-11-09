28ecc5ab03cbee21f2bf96abc8349b8310b3b0ad
Isaac Rovira <iroviraro@uoc.edu>
2022-11-10 00:22:32 +0100

    Counter class.

---
+++ app/src/main/java/com/example/puzzledroid/Counter.java
@@ -0,0 +1,25 @@
+package com.example.puzzledroid;
+
+public class Counter {
+    private int movements;
+
+    public Counter() {
+        this.movements = 0;
+    }
+    public void add(){
+        movements++;
+    }
+    public void add(int qty){
+        movements+=qty;
+    }
+    public void reset(){
+        movements = 0;
+    }
+
+    public int getMovements() {
+        return movements;
+    }
+    public void setMovements(int movements) {
+        this.movements = movements;
+    }
+}
